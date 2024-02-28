e3e87e46d51430efaefebd19a14254132cf5701c
Kiệt <dkiet27@gmail.com>
2024-02-28 13:32:42 +0700

    init

---
+++ src/View/MainView.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+public class MainView {
+    private JPanel MainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainView");
+        frame.setContentPane(new MainView().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
